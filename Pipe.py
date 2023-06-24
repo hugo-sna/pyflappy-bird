733a41651fdc925a2393bb8c4a51354dd86a537e
Hugo <133796530+hugo-sna@users.noreply.github.com>
2023-06-24 21:35:31 +0200

    Update Pipe.py
    Remade the indentation

--- Pipe.py
+++ Pipe.py
@@ -8,17 +8,14 @@ class Pipe:
     rectTop: Rectangle   = None
     rectBottom: Rectangle = None
 
-
     def __init__(self) -> None:
         self.openingTop    = random.randint(20, 50)
         self.openingBottom = random.randint(20, 50)
         self.rectTop: Rectangle    = Rectangle(320, -180 - self.openingTop, 75, 180)
-        self.rectBottom: Rectangle = Rectangle(320, 10 + self.openingBottom, 75, 180)
-        
-    
+        self.rectBottom: Rectangle = Rectangle(320, 10 + self.openingBottom, 75, 180)   
+
     def update(self) -> None:
         self.rectTop.x    -= 1
         self.rectBottom.x -= 1
         draw_rectangle_rec(self.rectTop, GREEN) # top rectangle
-        draw_rectangle_rec(self.rectBottom, GREEN) # bottom rectangle
-        
+        draw_rectangle_rec(self.rectBottom, GREEN) # bottom rectangle        
